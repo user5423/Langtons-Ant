820e147ba790f675d95dec731e0b56476fecb200
user5423 <davide.correia@outlook.com>
2021-12-26 23:14:56 +0000

    Added tests for apply_generalized_rule

--- tests/test.c
+++ tests/test.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 #include "langton.h"
 #include "visualiser.h"
+
+void setup(){
+   // Due to how we declare max_x and max_y we need to define them
+   // otherwise there may be garbage 0 and 1 values
+   extern int max_x, max_y;
+   max_x = 50;
+   max_y = 50;
+}
 
 // Test turn left by performing a full rotation of the ant
 void test_turn_left() {
@@ -39,6 +48,7 @@ void test_move_forward() {
    ant.x = 5;
    ant.y = 5;
    ant.direction = RIGHT;
+
    move_forward(&ant);
    assert(ant.x == 6 && ant.y == 5);
    ant.direction = LEFT;
@@ -50,15 +60,27 @@ void test_move_forward() {
    ant.direction = DOWN;
    move_forward(&ant);
    assert(ant.x == 5 && ant.y == 5);
-   //TODO: Test on edges
+  
 
+   // Additional Test Cases: Boundary Test Cases
    ant.x = 0;
    ant.y = 0;
+
    ant.direction = LEFT;
-   // assert
+   move_forward(&ant);
+   assert(ant.x == max_x && ant.y == 0);
 
    ant.direction = RIGHT;
-   // assert
+   move_forward(&ant);
+   assert(ant.x == 0 && ant.y == 0);
+
+   ant.direction = DOWN;
+   move_forward(&ant);
+   assert(ant.x == 0 && ant.y == max_y);
+
+   ant.direction = UP;
+   move_forward(&ant);
+   assert(ant.x == 0 && ant.y == 0);
    
 }
 
@@ -66,25 +88,99 @@ void test_move_forward() {
 // Test apply rule
 void test_apply_rule() {
    struct ant ant;
+   cell c;
    ant.x = 5;
    ant.y = 5;
    ant.direction = RIGHT;
-   cell c = WHITE;
+   
+   c = WHITE;
    apply_rule(&c, &ant);
    assert(ant.direction == DOWN);
    assert(c == BLACK);
+
    c = BLACK;
    apply_rule(&c, &ant);
    assert(ant.direction == RIGHT);
    assert(c == WHITE);
 }
 
+void test_generalized_apply_rule() {
+   struct ant ant;
+   int c;
+   ant.x = 5;
+   ant.y = 5;
+   ant.direction = RIGHT;
+
+   char *states;
+   int states_length;
+
+   // Edge test cases
+   c = 0;
+   states = "L";
+   states_length = strlen(states);
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == UP);
+   assert(c == 0);
+
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == LEFT);
+   assert(c == 0);
+
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == DOWN);
+   assert(c == 0);
+
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == RIGHT);
+   assert(c == 0);
+
+   // Edge test cases
+   c = 0;
+   states = "R";
+   states_length = strlen(states);
+   ant.direction = RIGHT;
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == DOWN);
+   assert(c == 0);
+
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == LEFT);
+   assert(c == 0);
+
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == UP);
+   assert(c == 0);
+
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(ant.direction == RIGHT);
+   assert(c == 0);
+
+
+   // Testing whether state loopback works
+   states = "RRRRR";
+   states_length = strlen(states);
+   ant.direction = RIGHT;
+
+   c = states_length - 1;
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(c == 0);
+
+   c = 0;
+   apply_generalized_rule(&c, &ant, states, states_length);
+   assert(c == 1);
+}
+
+
 
 
 int main() {
+   setup();
 	test_turn_right();
 	test_turn_left();
 	test_move_forward();
 	test_apply_rule();
+   // Additional tests for generalized langton's ant
+   test_generalized_apply_rule();
+   printf("-> Tests succesfully ran\n");
 }
 
